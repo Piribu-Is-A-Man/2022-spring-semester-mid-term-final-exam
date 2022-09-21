3b97629f98e1d49ef7a28651fdb9b6fa7c7afbdb
Kyungjun Kim <kkj3102@gmail.com>
2022-09-21 23:00:12 +0900

    first commit

---
+++ 2022spring/test1.py
@@ -0,0 +1,7 @@
+for celsius in range(0,51,5):
+    fahrenheit = (9/5)*celsius + 32
+    print('섭씨온도 : ', end='')
+    print(celsius)
+    print('화씨온도 : ', end='')
+    print(fahrenheit)
+    print()
